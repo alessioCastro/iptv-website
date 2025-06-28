b4c32cbaeade840ab7e9da486524e658ccb54bfe
alessioCastro <160541955+alessioCastro@users.noreply.github.com>
2025-06-27 22:54:26 -0400

    Update load-data.js

--- js/load-data.js
+++ js/load-data.js
@@ -1,6 +1,9 @@
-fetch('https://alessiocastro.github.io/iptv-website/json/part1.json')
-    .then(response => {
-        response.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.error('Error fetching JSON:', error));
+const origin = location.origin;
+
+async function test() {
+    var response = await fetch(origin + '/iptv-website/json/part1.json')
+    const data = await response.json();
+    return data;
+}
+
+test().then(response => console.log(response));

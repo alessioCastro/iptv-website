f2ac47e100f7c64714e65ab047b84c9efb4c103f
alessioCastro <160541955+alessioCastro@users.noreply.github.com>
2025-06-27 23:20:05 -0400

    Update load-data.js

--- js/load-data.js
+++ js/load-data.js
@@ -3,6 +3,7 @@ const origin = location.origin;
 async function test() {
     var response = await fetch(origin + '/iptv-website/json/part1.json')
     const data = await response.json();
+    document.body.innerText = JSON.stringify(data)
     return data;
 }
 
